5dade0ce980442f574c881cfa63295a653998867
Fl1ckerxD <mihaylav.slava@yandex.ru>
2022-05-23 16:36:41 +0300

    Add new pages AddDiscipline, AddFacultet

---
+++ Institute/Frames/Insert/AddDiscipline.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Institute.Classes;
+
+namespace Institute.Frames.Insert
+{
+    /// <summary>
+    /// Логика взаимодействия для AddDiscipline.xaml
+    /// </summary>
+    public partial class AddDiscipline : Page
+    {
+        public AddDiscipline()
+        {
+            InitializeComponent();
+        }
+
+        private void b_addNew_Click(object sender, RoutedEventArgs e)
+        {
+            Model.Дисциплина dis = new Model.Дисциплина()
+            {
+                Название = tb_title.Text
+            };
+            ConnectionDB.conDB.Дисциплина.Add(dis);
+            ConnectionDB.conDB.SaveChanges();
+        }
+    }
+}
---
+++ Institute/Frames/Insert/AddFacultet.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Institute.Classes;
+
+namespace Institute.Frames.Insert
+{
+    /// <summary>
+    /// Логика взаимодействия для AddFacultet.xaml
+    /// </summary>
+    public partial class AddFacultet : Page
+    {
+        public AddFacultet()
+        {
+            InitializeComponent();
+        }
+
+        private void b_addNew_Click(object sender, RoutedEventArgs e)
+        {
+            Model.Факультет facult = new Model.Факультет()
+            { 
+                Название = tb_title.Text
+            };
+            ConnectionDB.conDB.Факультет.Add(facult);
+            ConnectionDB.conDB.SaveChanges();
+        }
+    }
+}
